073f545e3277c96dcbf0662711151977f6fa4d0d
Agustin Di Nezio <adineziocamiletti@gmail.com>
2022-11-03 18:31:57 -0300

    "Ejercicio 7 terminado"

--- Entidades/Models.Derivados/Paciente.cs
+++ Entidades/Models.Derivados/Paciente.cs
@@ -1,4 +1,5 @@
-﻿using Entidades.Model.Base;
+﻿using Entidades.Ayudantes;
+using Entidades.Model.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,18 +11,34 @@ namespace Entidades.Models.Derivados
     public class Paciente:Persona
     {
         #region constructor
-        public Paciente(string nroHistoriaClinica,int id, string nombre, string apellido, string domicilio, string telefono, string email) : base(id, nombre, apellido, domicilio, telefono, email)
+        public Paciente(DateTime fechaNacimiento ,string nroHistoriaClinica,int id, string nombre, string apellido, string domicilio, string telefono, string email) : base(id, nombre, apellido, domicilio, telefono, email)
         {
+            FechaNacimiento = fechaNacimiento;
             NroHistoriaClinica = nroHistoriaClinica;
+            Edad = UtilidadesFechas.CalcularEdad(FechaNacimiento.Year);
         }
 
         #endregion
 
         #region propiedades
+        public DateTime FechaNacimiento { get; set; }
         public string NroHistoriaClinica { get; set; }
+
+        private int Edad;
+
+        public int edad
+        {
+            get { return Edad; }
+          
+        }
+
 
         #endregion
 
+
+
+       
+        
 
     }
 }
--- Negocio/AdmPaciente.cs
+++ Negocio/AdmPaciente.cs
@@ -14,22 +14,20 @@ namespace Negocio
         public static List<Paciente> Listar()
         {
             pacientes = new List<Paciente>();
-            Paciente paciente1 = new Paciente("66258221",120,"Juan","Carlos","Sarmiento 1325","4786599","juancito@gmail.com");
-            pacientes.Add(paciente1);
-            pacientes.Add(new Paciente("33218221", 121, "Tomas", "Fanta", "Roca 1125", "4986144", "fantajuan@gmail.com"));
-            pacientes.Add(new Paciente("11123548", 122, "Francisco", "Niebla", "Saavedra 100", "4735595", "francisconiebla@gmail.com"));
-            pacientes.Add(new Paciente("33345545", 123, "Guido", "Gonzales", "Alem 1200", "2236984532", "guidogon@gmail.com"));
-            pacientes.Add(new Paciente("66258221", 120, "Patricia", "Perez", "Francia 2143", "2234879362", "perezpatricia@gmail.com"));
+            pacientes.Add(new Paciente(new DateTime(1994,7,8), "66258221", 120, "Juan", "Carlos", "Sarmiento 1325", "4786599", "juancito@gmail.com"));
+            pacientes.Add(new Paciente(new DateTime(1995,8,25), "33218221", 121, "Tomas", "Fanta", "Roca 1125", "4986144", "fantajuan@gmail.com"));
+            pacientes.Add(new Paciente(new DateTime(2000,1,24), "11123548", 122, "Francisco", "Niebla", "Saavedra 100", "4735595", "francisconiebla@gmail.com"));
+            pacientes.Add(new Paciente(new DateTime(1980,5,15), "33345545", 123, "Guido", "Gonzales", "Alem 1200", "2236984532", "guidogon@gmail.com"));
+            pacientes.Add(new Paciente(new DateTime(2012,12,1), "66258221", 124, "Patricia", "Perez", "Francia 2143", "2234879362", "perezpatricia@gmail.com"));
             return pacientes;
         }
-
 
         public static int Insertar(Paciente paciente)
         {
             return 0;
         }
 
-        public static bool Eliminar(int id)           // En realidad la funcion retorna un int
+        public static bool Eliminar(int id)    // En realidad la funcion retorna un int
         {
             bool flag = false;
             foreach (var item in pacientes)
